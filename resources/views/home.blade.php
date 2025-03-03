a21953e6317a0eb87de04f65b7f076eb451ca71c
Manos Baumer <manos.baumer@icloud.com>
2025-03-03 14:43:28 +0100

    aanpassingen nav

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body style="background-color: #000000; padding: 0px;">
+<body style=" background: url('/images/background.png') no-repeat center center fixed;
+            background-size: cover; padding: 0px;">
     @include('layouts.nav')
 </body>
 </html>
