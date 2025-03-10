3cf5beaa1eba8fdd28432f8984682895baba1895
Manos Baumer <manos.baumer@icloud.com>
2025-03-10 14:21:37 +0100

    exhibits show werkend

--- resources/views/exhibit/show.blade.php
+++ resources/views/exhibit/show.blade.php
@@ -7,28 +7,28 @@
     @foreach($exhibit->multimedia as $media)
         <p>Image:
             @if($media->image)
-                <img src="{{ asset('storage/public/images/' . $media->image) }}" alt="Image">
+                <img src="{{ asset('images/' . $media->image) }}" alt="Image">
             @else
                 No image
             @endif
         </p>
         <p>Video:
             @if($media->video)
-                <video src="{{ asset('storage/public/videos/' . $media->video) }}" controls></video>
+                <video src="{{ asset('videos/' . $media->video) }}" controls></video>
             @else
                 No video
             @endif
         </p>
         <p>Audio:
             @if($media->audio)
-                <audio src="{{ asset('storage/public/audios/' . $media->audio) }}" controls></audio>
+                <audio src="{{ asset('audios/' . $media->audio) }}" controls></audio>
             @else
                 No audio
             @endif
         </p>
         <p>QR Code:
             @if($media->qrcode)
-                <img src="{{ asset('storage/qrcodes/' . $media->qrcode) }}" alt="QR Code">
+                <img src="{{ asset('qrcodes/' . $media->qrcode) }}" alt="QR Code">
             @else
                 No QR Code
             @endif
